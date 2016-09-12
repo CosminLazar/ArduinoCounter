1a4d63c2f5f98fd8ef2e14ae3ef67bdbcc343568
Cosmin Lazar <cosminconstantinlazar@gmail.com>
2016-09-12 23:31:28 +0200

    Make conversion table static

--- DigitDisplay.h
+++ DigitDisplay.h
@@ -34,52 +34,8 @@ private:
 	static const uint8_t LastSupportedCharacter = FirstSupportedCharacter + ConversionTableLength - 1;
 	static const uint8_t SegmentON = LOW;
 	static const uint8_t SegmentOFF = HIGH;
+	static const uint8_t ConversionTable[ConversionTableLength];
 
-	uint8_t ConversionTable[ConversionTableLength] = {
-		/* [0] - 0		*/ 126,
-		/* [1] - 1		*/ 48,
-		/* [2] - 2		*/ 109,
-		/* [3] - 3		*/ 121,
-		/* [4] - 4		*/ 51,
-		/* [5] - 5		*/ 91,
-		/* [6] - 6		*/ 95,
-		/* [7] - 7		*/ 114,
-		/* [8] - 8		*/ 127,
-		/* [9] - 9		*/ 123,
-		/* [10] - :		*/ 72,
-		/* [11] - ;		*/ 88,
-		/* [12] - <		*/ 67,
-		/* [13] - =		*/ 9,
-		/* [14] - >		*/ 97,
-		/* [15] - ?		*/ 101,
-		/* [16] - @		*/ 125,
-		/* [17] - A		*/ 119,
-		/* [18] - B		*/ 127,
-		/* [19] - C		*/ 78,
-		/* [20] - D		*/ 61,
-		/* [21] - E		*/ 79,
-		/* [22] - F		*/ 71,
-		/* [23] - G		*/ 94,
-		/* [24] - H		*/ 55,
-		/* [25] - I		*/ 6,
-		/* [26] - J		*/ 60,
-		/* [27] - K		*/ 87,
-		/* [28] - L		*/ 14,
-		/* [29] - M		*/ 84,
-		/* [30] - N		*/ 118,
-		/* [31] - O		*/ 126,
-		/* [32] - P		*/ 103,
-		/* [33] - Q		*/ 107,
-		/* [34] - R		*/ 102,
-		/* [35] - S		*/ 91,
-		/* [36] - T		*/ 15,
-		/* [37] - U		*/ 62,
-		/* [38] - V		*/ 62,
-		/* [39] - W		*/ 42,
-		/* [40] - X		*/ 55,
-		/* [41] - Y		*/ 59,
-		/* [42] - Z		*/ 109
-	};
 	Segments _segmentSetup;
 public:
 	DigitDisplay() {};
