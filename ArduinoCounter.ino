99d70e838d7793868241c367cd778c9b684db81b
Cosmin Lazar <cosminconstantinlazar@gmail.com>
2016-09-12 23:22:34 +0200

    Add increase/decrease controls

--- ArduinoCounter.ino
+++ ArduinoCounter.ino
@@ -1,3 +1,4 @@
+#include "PushButton.h"
 #include "AutoScrollingDisplay.h"
 #include "DigitDisplayDecorator.h"
 #include "MultiplexedDisplay.h"
@@ -10,13 +11,29 @@ DigitDisplay display(segmentConfig);
 
 AutoScrollingDisplayClass * sdisplay = new AutoScrollingDisplayClass(new MultiplexedDisplay(&display, segmentConfig, digitSelectors, 2), 2, 200);
 
+PushButtonClass increase(2, LOW);
+PushButtonClass decrease(3, LOW);
+uint8_t currentNumber = 0;
+
 void setup()
-{	
+{
+	pinMode(3, INPUT_PULLUP);
+	pinMode(2, INPUT_PULLUP);
 	sdisplay->Display("123456789");
 }
 
 void loop()
 {
+	increase.Process();
+	decrease.Process();
 	sdisplay->Process();
+
+	if (increase.IsDepressed()) {
+		currentNumber = currentNumber == 256 ? 256 : ++currentNumber;
+	}
+	else if (decrease.IsDepressed()) {
+		currentNumber = currentNumber == 0 ? 0 : --currentNumber;;
+	}
+	sdisplay->Display(String(currentNumber));
 	delay(10);
 }
